cff55381f4a755fe61a9dd57bbd2af230d2c833f
NasC0 <Spirital@gmail.com>
2013-03-05 13:22:26 +0200

    Finished version attribute class

---
+++ C# OOP/2. DeclaringClassesPartII/11. VersionAttrivuteTest/VersionAttrivuteTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Version;
+
+[Version(1, 1)]
+class VersionAttributeTest
+{
+    static void Main()
+    {
+        Type type = typeof(VersionAttributeTest);
+        object[] versionAttribute = type.GetCustomAttributes(false);
+        foreach (VersionAttribute version in versionAttribute)
+        {
+            Console.WriteLine("Version: {0}", version.FullVersion);
+        }
+    }
+}
---
+++ C# OOP/2. DeclaringClassesPartII/Version/Version.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Version
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface | AttributeTargets.Enum | AttributeTargets.Method)]
+    public class VersionAttribute : System.Attribute
+    {
+        private int major;
+        private int minor;
+        private string fullVersion;
+
+        public VersionAttribute(int major, int minor)
+        {
+            this.major = major;
+            this.minor = minor;
+            this.fullVersion = major.ToString() + "." + minor.ToString();
+        }
+
+        public int Major
+        {
+            get { return this.major; }
+        }
+
+        public int Minor
+        {
+            get { return this.minor; }
+        }
+
+        public string FullVersion
+        {
+            get { return this.fullVersion; }
+        }
+    }
+}
